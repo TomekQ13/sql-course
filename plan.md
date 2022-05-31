60a1fb127b1ed3684421221b03d708bb1153d121
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-05-31 20:36:59 +0200

    initial commit

---
+++ plan.md
@@ -0,0 +1,99 @@
+# SQL For Data Analysts and Data Scientists
+
+<details>
+    <summary>1. Introduction to SQL</summary>
+
+    - SQL - Structured Query Language
+    - Used to work (build, access and change data) with relational databases
+    - Standardized by ISO and ANSI (with small differences between different DBMS)
+</details>
+
+<details>
+    <summary>2. Environment setup</summary>
+  
+    - Oracle
+    - SQL Developer
+</details>
+
+<details>
+    <summary>3. Select</summary>
+  
+    - Columns
+    - Aliases
+    - Where
+    - Aggregation functions
+    - Group by
+    - Having
+</details>
+
+<details>
+    <summary>4. Joins</summary>
+  
+    - Left/Right
+    - Inner
+    - Combinations
+    - Aliases
+</details>
+
+
+<details>
+    <summary>5. Subqueries</summary>
+  
+    - select ... where ... in ()
+</details>
+
+<details>
+    <summary>6. Create Table</summary>
+  
+    - Data types
+    - Constraints
+    - Relations
+    - 
+</details>
+
+<details>
+    <summary>7. Views</summary>
+  
+</details>
+
+<details>
+    <summary>8. Character functions</summary>
+  
+</details>
+
+<details>
+    <summary>9. Numeric functions</summary>
+
+</details>
+
+<details>
+    <summary>10. Date/timestamp functions</summary>
+  
+</details>
+
+<details>
+    <summary>11. Data Manipulation Language statements</summary>
+  
+  - Insert
+  - Delete
+  - Update
+  - Truncate
+</details>
+
+<details>
+    <summary>12. Performance optimization basics</summary>
+  
+  - Indexes
+  - Partitions
+  - Explain plan
+  - No functions and implicit casts
+</details>
+
+<details>
+    <summary>13. Small topics</summary>
+  
+  - Self join
+  - Union
+</details>
+
+
---
+++ test_data_countries_people_2.sql
@@ -0,0 +1,69 @@
+drop table countries;
+
+create table countries (
+    name varchar2(255),
+    area_km2 integer,
+    currency varchar2(255)
+);
+
+insert into countries
+values ('Germany', 357588, 'Euro');
+
+insert into countries
+values ('USA', 9834000, 'US Dollar');
+
+insert into countries
+values ('Poland', 322575, 'Zloty');
+
+insert into countries
+values ('Switzerland', 41285, 'Swiss frank');
+
+insert into countries
+values ('France', 543940, 'Euro');
+
+commit;
+
+drop table people;
+
+create table people (
+    id integer,
+    name varchar2(255),
+    surname varchar2(255),
+    date_of_birth date,
+    favourite_color varchar2(255),
+    country varchar2(255),
+	years_experience integer,
+	cost_living integer
+);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (1, 'Penelope' ,'Smith', '11-SEP-1922', 'green', 'Germany', 3, 1000);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (2, 'Adam' ,'Johnson', '11-DEC-2007', 'red', 'Switzerland', 1, 500);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (3, 'John' ,'Smith', '01-FEB-2001', 'black', 'Poland', 10, 2500);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (4, 'Evelyn' ,'Doe', '11-NOV-1999', 'blue', 'USA', 9, 3000);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (5, 'Clayton' ,'Bradford', '02-DEC-1997', 'green', 'Germany', 15, 8000);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (6, 'Jena' ,'Hartman', '11-NOV-1996', 'green', 'Poland', 5, 2000);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (7, 'Louis' ,'Hopkins', '07-APR-2004', 'red', 'USA', 1, 500);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (8, 'Sebastian' ,'Carson', '18-AUG-1999', 'blue', 'Germany', 12, 7900);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (9, 'Jena' ,'Carson', '09-APR-1999', 'blue', 'USA', 7, 10000);
+
+insert into people (id, name, surname, date_of_birth, favourite_color, country, years_experience, cost_living)
+values (10, 'Johnny' ,'Smith', '07-JUL-1999', 'blue', 'Denmark', 17, 11000);
+
+commit;
